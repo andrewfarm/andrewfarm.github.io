b2870aee999aee0c7a5225eb7b8a9c354271d708
andrewfarm <Andrew.Farm@tufts.edu>
2016-10-28 20:29:31 -0400

    Add omg title text via jQuery

--- gallery/titletextloader.js
+++ gallery/titletextloader.js
@@ -1,9 +1,9 @@
-var loadAltText = function() {
+var loadTitleText = function() {
         var $thumbnails = $("#thumbnails").children();
         for (var i = 0; i < $thumbnails.length; i++) {
                 var $img = $thumbnails.eq(i).children().first();
                 var filename = getFileName($img.attr("src"));
-                $img.attr("alt", filename);
+                $img.attr("title", filename);
         }
 };
 
@@ -11,4 +11,4 @@ var getFileName = function(path) {
         return path.substring(path.lastIndexOf("/") + 1, path.lastIndexOf("."));
 };
 
-$(document).ready(loadAltText);
+$(document).ready(loadTitleText);
