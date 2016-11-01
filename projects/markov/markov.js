9b96ab8f77ef0ef62869f44a68fabeb362136a55
andrewfarm <Andrew.Farm@tufts.edu>
2016-11-01 01:22:19 -0400

    Improve script, split text into unique symbols

--- projects/markov/markov.js
+++ projects/markov/markov.js
@@ -3,13 +3,57 @@ function run() {
         var degrees = parseInt($("#degrees").val());
         var text = $("#trainingtext").val();
         var length = parseInt($("#length").val());
-        var output = doMarkov(method, degrees, text, length);
-        alert(output);
+        
+        var $genButton = $("#generate");
+        $genButton.remove();
+        $("#clear").before("<span id=\"loading\"><img align=\"middle\" src=\"loading.gif\" width=\"32px\"/>\n<span id=\"status\">Analyzing...</span></span>");
+        
+        setTimeout(function() {
+                   var output = doMarkov(method, degrees, text, length);
+                   $("#loading").remove();
+                   $("#clear").before($genButton);
+                   $("#output").text(output);
+        }, 0);
 }
 
 var doMarkov = function(method, degrees, text, length) {
-        return method + "\n" + degrees + "\n" + text.substring(0, 10) + "\n" + length;
+        //Break up text into symbols
+        var split;
+        if (method === "byLetters") {
+                split = "";
+        } else {
+                split = " ";
+        }
+        var textAsSymbols = text.split(split);
+        var symbols = uniq(textAsSymbols);
+        var strings = [];
+        //Analyze symbol combinations
+//        for (var i = 0; i < textAsSymbols.length - 1; i++) {
+//                
+//        }
+        $("#status").text("Generating...");
+        //Generate text
+        return symbols;
 };
+
+var uniq = function(list) {
+        var uniqList = [];
+        for (var i = 0; i < list.length; i++) {
+                if (!contains(uniqList, list[i])) {
+                        uniqList.push(list[i]);
+                }
+        }
+        return uniqList;
+}
+
+var contains = function(list, search) {
+        for (var i = 0; i < list.length; i++) {
+                if (list[i] === search) {
+                        return true;
+                }
+        }
+        return false;
+}
 
 $(document).ready(function() {
         $("#generate").click(run);
