046cbfe4563f65099ca75abb659860d228d25f34
andrewfarm <Andrew.Farm@tufts.edu>
2016-11-01 21:52:54 -0400

    Remove polyhack project from live site

--- projects/polyhack2016/barkr/index.js
+++
@@ -1,7 +0,0 @@
-var init = function() {
-        $("#login").click(function() {
-                           document.location.href = "dashboard.html";
-                           });
-}
-
-$(document).ready(init);
